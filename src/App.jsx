8f37dcd42b4d7cd34589f02951576d547e9ceb7e
Huu Vuong Phan <166980411+yangihoffi@users.noreply.github.com>
2025-01-10 15:27:18 +0700

    Add note component and show placeholders

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,34 @@
+import Note from "./components/Note";
+
+const notes = [
+  {
+    id: "1",
+    content: "HTML is easy",
+    important: true,
+  },
+  {
+    id: "2",
+    content: "Browser can execute only JavaScript",
+    important: false,
+  },
+  {
+    id: "3",
+    content: "GET and POST are the most important methods of HTTP protocol",
+    important: true,
+  },
+];
+
 const App = () => {
-  return <div></div>;
+  return (
+    <div>
+      <h1>Notebook</h1>
+      <ul>
+        {notes.map((note) => (
+          <Note key={note.id} note={note} />
+        ))}
+      </ul>
+    </div>
+  );
 };
 
 export default App;
